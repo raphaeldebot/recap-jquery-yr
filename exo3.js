ac8cecce93915628869b234d1519465d93a10a5b
raphaeldebot <debot.raphael@gmail.com>
2026-05-11 16:01:34 +0200

    modification des animation

--- exo3.js
+++ exo3.js
@@ -1,71 +1,59 @@
-// DARK MODE 
-$(".dark-mode-js").on("click",function(){
-   $("body").toggleClass("bodyDarkMode")
-})
+// DARK MODE
+$(".dark-mode-js").on("click", function () {
+  $("body").toggleClass("bodyDarkMode");
+});
 // -------------------
 // MENU
 
 // -------------------
 // CARDS
-$(".card-01-js").on("click",function(){
-   $(this).animate({bottom:"20px"},2000,function(){
-      $(this).animate({bottom:"0"},2000)
-   })
-})
-$(".card-02-js").on("click",function(){
-   
-})
-$(".card-03-js").on("click",function(){
-  
-})
+$(".card-01-js").on("click", function () {
+  $(this).animate({ bottom: "20px" }, 600).animate({ bottom: "0" }, 300);
+});
 
+$(".card-02-js").on("click", function () {
+  $(this).animate({ bottom: "20px" }, 600).animate({ bottom: "0" }, 300);
+});
 
+$(".card-03-js").on("click", function () {
+  $(this).animate({ bottom: "20px" }, 600).animate({ bottom: "0" }, 300);
+});
 
 // -------------------
 
 // CONTROLS
-$("#carte").click(function(){
-        $(this).css("border","1px solid blue");
-       });
+$("#lancer").click(function () {
+  $(".box1")
+    .animate({ left: "100px" })
+    .animate({ top: "100px" }, function () {
+      $(".box1").css("backgroundColor", "red");
+    })
+    .animate({ left: "0px" })
+    .animate({ top: "0px" });
+});
 
+$("#stop").click(function () {
+  $(".box1").stop();
+});
 
-$("#lancer").click(function(){
-   $(".box1").animate({left:"100px"})
-   .animate({top:"100px"},function(){
-      $(".box1").css("backgroundColor","red")
-   })
-   .animate({left:"0px"})
-   .animate({top:"0px"})
-})
+$(".box1").click(function () {
+  $(this)
+    .animate({ left: "+=100px" })
+    .animate({ top: "+=100px" }, function () {
+      $(this).css("backgroundColor", "red");
+    })
+    .animate({ left: "-=100px" })
+    .animate({ top: "-=100px" });
+});
 
-$("#stop").click(function() {
-   $(".box1").stop();
-})
+$(".box2").click(function () {
+  $(this).animate({ BorderRadius: "50%" });
+});
 
+$(".box3").click(function () {
+  $(this).animate({ left: "100px" });
+});
 
-       
-$(".box1").click(function(){
-   $(this).animate({left:"+=100px"})
-   .animate({top:"+=100px"},function(){
-      $(this).css("backgroundColor","red")
-   })
-   .animate({left:"-=100px"})
-   .animate({top:"-=100px"})
-})
-
-
-
-$(".box2").click(function(){
-   $(this).animate({BorderRadius:"50%"})
-   
-})
-
-$(".box3").click(function(){
-   $(this).animate({left:"100px"})
-   
-})
-
-$(".box4").click(function(){
-   $(this).animate({rotate:"+=360deg"})
-   
-})
+$(".box4").click(function () {
+  $(this).animate({ rotate: "+=360deg" });
+});
