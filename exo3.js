507e1dee978ac870c43e81dfb3f830e23ae9bd14
raphaeldebot <debot.raphael@gmail.com>
2026-05-11 15:43:49 +0200

    modification du js

--- exo3.js
+++ exo3.js
@@ -7,11 +7,44 @@ $("#carte").click(function(){
         $(this).css("border","1px solid blue");
        });
 
-$(".box1").click(function(){
-   $(this).animate({left:"100px"})
+
+$("#lancer").click(function(){
+   $(".box1").animate({left:"100px"})
    .animate({top:"100px"},function(){
-      $(this).css("backgroundColor","red")
+      $(".box1").css("backgroundColor","red")
    })
    .animate({left:"0px"})
    .animate({top:"0px"})
+})
+
+$("#stop").click(function() {
+   $(".box1").stop();
+})
+
+
+       
+$(".box1").click(function(){
+   $(this).animate({left:"+=100px"})
+   .animate({top:"+=100px"},function(){
+      $(this).css("backgroundColor","red")
+   })
+   .animate({left:"-=100px"})
+   .animate({top:"-=100px"})
+})
+
+
+
+$(".box2").click(function(){
+   $(this).animate({BorderRadius:"50%"})
+   
+})
+
+$(".box3").click(function(){
+   $(this).animate({left:"100px"})
+   
+})
+
+$(".box4").click(function(){
+   $(this).animate({rotate:"+=360deg"})
+   
 })
