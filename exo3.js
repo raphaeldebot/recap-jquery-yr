a62f9e4cb399b48e5901b2bcc20049438f7a5d1e
raphaeldebot <debot.raphael@gmail.com>
2026-05-11 14:26:30 +0200

    animation box 1 et ajout des consigne (readme)

--- exo3.js
+++ exo3.js
@@ -1,8 +1,17 @@
 
 $(".dark-mode-js").on("click",function(){
-   $("body").toggleClass("bodyDarkMode")
-})
+   $("body").toggleClass("bodyDarkMode");
+});
 
 $("#carte").click(function(){
         $(this).css("border","1px solid blue");
-       })
+       });
+
+$(".box1").click(function(){
+   $(this).animate({left:"100px"})
+   .animate({top:"100px"},function(){
+      $(this).css("backgroundColor","red")
+   })
+   .animate({left:"0px"})
+   .animate({top:"0px"})
+})
